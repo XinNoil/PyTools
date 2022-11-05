10f2a3f15b44a9b30ad136a8ac7919dc5b5ce95f
XinNoil <jiankunwang@tju.edu.cn>
2022-11-05 23:41:25 +0800

    modi sth

--- mtools/io.py
+++ mtools/io.py
@@ -24,16 +24,16 @@ def file_dir(file):
 def toobj(strjson):
     json.loads(strjson)
 
-def load_json(filename):
-    json_file=open(filename, 'r')
+def load_json(filename, encoding=None):
+    json_file=open(filename, 'r', encoding=encoding)
     json_strings=json_file.readlines()
     json_string=''.join(json_strings)
     json_file.close()
     return json.loads(json_string)
 
-def save_json(filename, obj, ensure_ascii=True):
+def save_json(filename, obj, ensure_ascii=True, encoding=None):
     str_json=tojson(obj, ensure_ascii)
-    with open(filename,'w') as f:
+    with open(filename, 'w', encoding=encoding) as f:
         f.write(str_json)
         f.close()
 
@@ -95,8 +95,8 @@ def write_file(file_name, str_list, encoding=None):
     file_.writelines(['%s\n'%s for s in str_list])
     file_.close()
 
-def read_file(file_name):
-    file_=open(file_name, 'r')
+def read_file(file_name, encoding=None):
+    file_=open(file_name, 'r', encoding=encoding)
     str_list = file_.read().splitlines()
     file_.close()
     return str_list
