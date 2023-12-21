b151bad91bffe32d60a8357e53c80009bb8e1bec
XinNoil <jiankunwang@tju.edu.cn>
2023-12-21 11:01:16 +0800

    modi sth on tjubd5

--- mtools/pd.py
+++ mtools/pd.py
@@ -1,2 +1,10 @@
+import pandas as pd
+
 def sort_df(df, by, custom_dict):
     return df.sort_values(by=[by], key=lambda x: x.map(custom_dict))
+
+def select_df(df, column, value):
+    if isinstance(value, list):
+        return df[df[column].isin(value)]
+    else:
+        return df[df[column]==value]
